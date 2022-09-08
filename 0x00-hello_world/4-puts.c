42a14511a15f8baf45262e0498d4bc72e75b08a7
Michael Aloh <mykelaloh@gmail.com>
2022-09-08 16:19:12 -0700

    created puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+/**
+	*	main - Entry point
+	*
+	*	Return: Always zero(success)
+	*/
+int main(void)
+{
+	char message[ ]="\"Programming is like building a multilingual puzzle\n";
+	puts(message);
+	return (0);
+}
