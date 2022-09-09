ae44ee9ead8f11bbb4c2e4ce1a6f91176bef9c20
Michael Aloh <mykelaloh@gmail.com>
2022-09-08 19:18:47 -0700

    uodated 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	long long int c;
 	char d;
 	float f;
-	printf("Size of a char: %d byte(s)\n",sizeof(d));
+	printf("Size of a char: %d byte(s)\n", sizeof(d));
 	printf("Size of an int: %d byte(s)\n", sizeof(a));
 	printf("Size of a long int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(c));
