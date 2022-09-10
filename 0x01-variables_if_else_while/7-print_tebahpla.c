de15b3f7e4062454ed24f96a5f47794041c57153
Michael Aloh <mykelaloh@gmail.com>
2022-09-09 20:52:33 -0700

    create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ * Description: To determine if number is greater\
+,less or equal to zero
+ * Return:Always 0 (success)
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 122; a <= 97; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
