ae9f18b6eb37723fd4921f4db7191365083da38d
Michael Aloh <mykelaloh@gmail.com>
2022-09-12 05:52:46 -0700

    changed the for loop condition range

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 48; a <= 57; a++)
+	for (a = 48; a <= 56; a++)
 	{
-		for (b = 48; b <= 57; b++)
+		for (b = 49; b <= 57; b++)
 		{
 			putchar(a);
 			putchar(b);
