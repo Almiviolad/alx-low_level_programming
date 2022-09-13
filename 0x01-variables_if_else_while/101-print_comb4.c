4a79740a18e4c4ded11139a81a485e9e5adcfa47
Michael Aloh <mykelaloh@gmail.com>
2022-09-13 02:34:48 -0700

    add an if condutonal statement remive equal last 2 digit and digit3<digit2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,22 +7,24 @@
  */
 int main(void)
 {
-	int digit0, digit1, digit2, a = 0, b = 1;
+	int digit0, digit1, digit2, b = 1;
 
 	for (digit0 = 0; digit0 < 8; digit0++)
 	{
-		a++;
 		b++;
 		for (digit1 = a; digit1 < 9; digit1++)
 		{
 			for (digit2 = b; digit2 < 10; digit2++)
 			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
+				if (digit != digit2 && digit2 > digit1)
+				{
+					putchar((digit1 % 10) + '0');
+					putchar((digit2 % 10) + '0');
+				}
 				if (digit0 == 7 && digit1 == 8 && digit2 == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
