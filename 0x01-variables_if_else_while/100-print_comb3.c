c60bfc855ca8d637301855f565a64ee32cc6d682
Michael Aloh <mykelaloh@gmail.com>
2022-09-12 23:03:02 -0700

    change dugit2 starting value

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	int digit1, digit2, a=0;
+	int digit1, digit2, a = 0;
 
 	for (digit1 = 0; digit1 < 10; digit1++)
 	{a++;
-		for (digit2 = 0; digit2 < 10; digit2++)
+		for (digit2 = a; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
