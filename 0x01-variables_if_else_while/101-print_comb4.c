4fd6fa4fd03271b77e18f9e8f3f543d89703d3c5
Michael Aloh <mykelaloh@gmail.com>
2022-09-13 02:58:42 -0700

    adjusted the last if condition

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,11 +21,11 @@ int main(void)
 					putchar((digit0 % 10) + '0');
 					putchar((digit1 % 10) + '0');
 					putchar((digit2 % 10) + '0');
+					if (digit0 == 7 && digit1 == 8 && digit2 == 9)
+						continue;
+					putchar(',');
+					putchar(' ');
 				}
-				if (digit0 == 7 && digit1 == 8 && digit2 == 9)
-					continue;
-				putchar(',');
-				putchar(' ');
 			}
 		}
 	}
