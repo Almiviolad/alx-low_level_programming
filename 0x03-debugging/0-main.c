32f92cf84955566cf9335eb1114ae565c3be5901
Michael Aloh <mykelaloh@gmail.com>
2022-09-14 16:07:36 -0700

    create main.h and 0-main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*test-test function that printa if number is positive or negative
+*
+* Return: 0
+*/
+
+int test(void)
+{
+	int i = 0;
+	
+	positive_or_negative(i);
+	return (0);
+}
