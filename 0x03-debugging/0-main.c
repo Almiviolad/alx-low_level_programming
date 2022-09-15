00ac0f0fdfb526d0603940826044fef0ede558de
Michael Aloh <mykelaloh@gmail.com>
2022-09-14 19:30:21 -0700

    create 0-main.c and main.h header file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
 *test-test function that printa if number is positive or negative
 *
@@ -8,8 +7,9 @@
 
 int test(void)
 {
-	int i = 0;
-	
+	int i;
+
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
