c4ee66790f25a12322a8bd2c4ff63da8e933520e
Michael Aloh <mykelaloh@gmail.com>
2022-09-14 20:04:16 -0700

    mad the files exexutable

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,7 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include "main.c"
+
 /**
 *main-test function that printa if number is positive or negative
-*
 * Return: 0
 */
 
@@ -15,5 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+	
 	return (0);
 }
