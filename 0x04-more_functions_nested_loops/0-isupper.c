046910fb67cfa00bd169a9963ce44c50fde80a0d
Michael Aloh <mykelaloh@gmail.com>
2022-09-15 14:47:35 -0700

    created 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_isupper-tells if a character is uppercase or not
+ *@c: character to be tested
+ *Return:1(upper) or 0(not upper)
+ */
+int _isupper(int c)
+{
+	int upper;
+
+	if (c >= 65 && c <= 90)
+	{
+		upper = 1;
+	}
+	else
+	{
+		upper = 0;
+	}
+	return (upper)
+}
