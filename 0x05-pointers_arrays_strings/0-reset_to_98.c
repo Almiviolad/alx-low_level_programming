78cd5a6957bbfcd05635de972135ae2792d69fb7
Michael Aloh <mykelaloh@gmail.com>
2022-09-19 09:52:38 -0700

    modify 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,10 @@
 /**
 *reset_to_98 - takes a pointer to an int as parameter
 *and updates the value it points to to 98.
-*@p: pointer to n
+*@n: pointer to n
 *Description: chamges the value a var contan to 98
 */
-void reset_to_98(int *p)
+void reset_to_98(int *n)
 {
-	int n;
-
-	p = &n;
-	*p = 98;
+	*n = 98;
 }
