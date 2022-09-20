8cc7ca776bf9a9acf02fbbceef55b5ef131341ba
Michael Aloh <mykelaloh@gmail.com>
2022-09-20 10:24:36 -0700

    modufy 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
-	while ((src[index] || src[index] == '\0'))
+	while (src [index])
 	{
 		dest[index] = src[index];
 		index++;
 	}
+	dest[index + 1] = src[index+1];
 
 	return (dest);
 }
