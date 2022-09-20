54d7371838b242529403af6d0afcb3dd6c6ebd2c
Michael Aloh <mykelaloh@gmail.com>
2022-09-20 10:04:56 -0700

    created 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strcpy - copies a string to another
+ *@dest: the file to be copied into
+ *@src: the source file to be copied from
+ *Return: *dest(the destination file)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+
+	return (dest);
+}
