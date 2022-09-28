e7953490d8b407ddefdc3d50ad02d698ad4234c7
Michael Aloh <mykelaloh@gmail.com>
2022-09-28 10:04:10 -0700

    created 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_rev_recursion - prints a string ib reverse
+ *Description: prints string in reversse
+ *@s: string pointer
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}x
