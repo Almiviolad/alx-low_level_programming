8b35609d36dc9110ed2317c93a840ad081b1403b
Michael Aloh <mykelaloh@gmail.com>
2022-09-29 04:17:43 -0700

    modify 5-sqrt_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,4 +8,5 @@ int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
 int _pow_recursion(int x, int y);
+int square(int n, int val);
 #endif /*MAIN_H*/
