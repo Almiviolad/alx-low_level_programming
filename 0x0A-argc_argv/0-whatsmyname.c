c60cc3f2d9d9aef7d4f3c3a52cc7d6b62487e4f2
Michael Aloh <mykelaloh@gmail.com>
2022-10-01 22:40:35 -0700

    created gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-whatsmyname.c -o mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: number of arguments
+ *@argv: array containg the argumnets
+ *Return: always 0(sucess)
+ */
+int main(int argc, const char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
