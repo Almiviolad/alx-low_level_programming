b46cf08262ccc9f9c88394e35484a2ff04cf61f3
Michael Aloh <mykelaloh@gmail.com>
2022-10-10 11:12:56 -0700

    crete 1-imit_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,11 @@
-#include <dog.h>
+#include "dog.h"
 #include <stdlib.h>
 /**
  *init_dog - initialise a structure variable
- *@d - struct for dog
- *@age - age of dog
- *@name - name of the dog
- *@owner - owner of the dog
+ *@d: struct for dog
+ *@age: age of dog
+ *@name: name of the dog
+ *@owner: owner of the dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
