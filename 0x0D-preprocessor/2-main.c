82613c3b14524c6e12cbf37b9659184d93253f37
Michael Aloh <mykelaloh@gmail.com>
2022-10-11 11:07:30 -0700

    create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - entry point of program that prints file its compiled from
+ *Return: always 0(sucess)
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
