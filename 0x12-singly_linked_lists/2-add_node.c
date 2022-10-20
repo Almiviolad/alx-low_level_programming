7b16164f1a190a3cae788ce709e6840e98298cc8
Michael Aloh <mykelaloh@gmail.com>
2022-10-20 08:54:44 -0700

    modify 2-add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,11 +9,21 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int i;
 
+	if (!str)
+		return (NULL);
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 	new_node->str = strdup(str);
+	for (i = 0; new_node->str[i] != '\0'; i++)
+	{
+	};
+	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
