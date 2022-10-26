eab9c33a933f2209e96ad3819548032116b47a09
Michael Aloh <mykelaloh@gmail.com>
2022-10-25 20:59:09 -0700

    modified

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,11 +10,10 @@ size_t listint_len(const listint_t *h)
 	int count = 0;
 
 	head = h;
-	while (head->next)
+	while (head != NULL)
 	{
 		count++;
 		head = head->next;
 	}
-	count++;
 	return (count);
 }
