c2168d177645ec2b65fcbc3ba8734a54345d136a
Michael Aloh <mykelaloh@gmail.com>
2022-10-25 21:35:11 -0700

    done

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,11 +17,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	}
 	new->n = n;
 	*head = new;
-	if (!*head)
-	{
-		new->next = NULL;
-		return (new);
-	}
 	new->next = *head;
 	return (new);
 }
