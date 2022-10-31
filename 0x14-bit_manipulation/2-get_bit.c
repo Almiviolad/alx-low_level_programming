5a19f623b0fb2a2f73abbc8a51dbf217dba89cc1
Michael Aloh <mykelaloh@gmail.com>
2022-10-31 00:32:45 -0700

    modify task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,7 +3,7 @@
 *get_bit - get the bit at indexth position
 *@n: the number to be changed to bits
 *@index:index of the bit to be returned
-*Return: bit at index(sucess) or 0(fail)
+*Return: bit at index(sucess) or -1(fail)
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	{
 		bit = n >> index;
 		bit_value = bit & 1;
-		if (!bit || !bit_value)
+		if (bit_value != 0 || bit_value != 1)
 			return (-1);
 		return (bit_value);
 	}
