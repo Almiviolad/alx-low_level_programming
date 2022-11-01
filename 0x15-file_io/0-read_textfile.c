6aa647900fcfe0cd768aba2160339074a121c120
Michael Aloh <mykelaloh@gmail.com>
2022-11-01 02:33:23 -0700

    add free memory

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,19 +9,31 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, size;
 	int wr;
-	char *c = malloc(letters * sizeof(char));
+	char *c;
 
-	if (!filename || letters > SIZE_MAX)
+	if (!filename)
+		return (0);
+	c = malloc(letters * sizeof(char));
+	if (c == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
+	{
+		free(c);
 		return (0);
+	}
 	size = read(fd, c, letters);
 	if (size < 0)
+	{
+		free(c);
 		return (0);
+	}
 	wr = write(STDOUT_FILENO, c, letters);
 	if (wr < 0 || wr != (int) letters)
+	{
+		free(c);
 		return (0);
+	}
 	close(fd);
 	return ((size_t) wr);
 }
