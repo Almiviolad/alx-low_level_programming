4603c29c508247075bbb28d87b201223caaa7653
Michael Aloh <mykelaloh@gmail.com>
2023-04-07 17:26:31 -0700

    add null cinditions

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,6 +9,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	return (ht->array[index]->value);
+	if (index < ht->size)
+		if (strcmp(ht->array[index]->key, key) == 0)
+			return (ht->array[index]->value);
+		else
+			return (NULL);
+	else
+		return (NULL);
 }
