a68ca4a6252cf8d8401e129012b40ecc962cdfeb
Michael Aloh <mykelaloh@gmail.com>
2023-04-07 18:04:45 -0700

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,11 +14,16 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
+	if (current == NULL)
+		return (NULL);
+	if (strcmp(current->key, key) == 0)
+		return (current->value);
 	while (current->next)
 	{
 		if (strcmp(current->key, key) == 0)
-			return (ht->array[index]->value);
+			return (current->value);
 		current = current->next;
 	}
+
 	return (NULL);
 }
