628c878a5567acdff8b654c8a9f5064847d2992e
Michael Aloh <mykelaloh@gmail.com>
2023-04-07 17:42:33 -0700

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,15 +8,17 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *current;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
-	if (index < ht->size)
-		if (strcmp(ht->array[index]->key, key) == 0)
+	current = ht->array[index];
+	while (current->next)
+	{
+		if (strcmp(current->key, key) == 0)
 			return (ht->array[index]->value);
-		else
-			return (NULL);
-	else
-		return (NULL);
+		current = current->next;
+	}
+	return (NULL);
 }
